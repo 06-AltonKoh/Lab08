49528b3b2523562232161dd1190bc1273ee55bb9
06-AltonKoh <jojiahao@gmail.com>
2021-10-20 21:19:16 +0800

    Spawn more Obstacle

--- 06_AltonKoh_Lab08/Assets/Scripts/Player.cs
+++ 06_AltonKoh_Lab08/Assets/Scripts/Player.cs
@@ -23,5 +23,12 @@ public class Player : MonoBehaviour
             transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
         }   
     }
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+
 }
--- 06_AltonKoh_Lab08/Assets/Scripts/Spawnerscript.cs
+++ 06_AltonKoh_Lab08/Assets/Scripts/Spawnerscript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour
 {
-    public GameObject SpawnObject;
+    public GameObject[] SpawnObject;
     float PositionY;
+    private int NumberOfObject;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +17,14 @@ public class Spawnerscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-
 
     }
 
     void SpawnObjects()
     {
         PositionY = Random.Range(4, -4f);
+        NumberOfObject = Random.Range(0, SpawnObject.Length);
         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
-        Instantiate(SpawnObject, transform.position, transform.rotation);
+        Instantiate(SpawnObject[NumberOfObject], transform.position, transform.rotation);
     }
 }
