10917f158f1f695c2198d30f564df7fa67998fe1
06-AltonKoh <jojiahao@gmail.com>
2021-10-20 21:11:12 +0800

    Restrict Player movement

--- 06_AltonKoh_Lab08/Assets/Scripts/Player.cs
+++ 06_AltonKoh_Lab08/Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject PlayerObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (PlayerObj.transform.position.y > -3.5 && PlayerObj.transform.position.y < 5.5)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
 
-        float verticalInput = Input.GetAxis("Vertical");
-
-        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
-
-      
-
+            transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
+        }   
     }
+    
 }
